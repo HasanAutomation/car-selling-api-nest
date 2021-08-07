51b96d547a795461f21a99a52452be87827aa5da
Hasan Ali <hasan.ali@lacritz.com>
2021-08-07 18:59:01 +0530

    Added query methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,29 @@
+import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('auth')
 export class UsersController {
 
+    constructor(private userService: UsersService) { }
+
     @Post('/signup')
     createUser(@Body() body: CreateUserDto) {
+        return this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(@Param('id') id: string) {
+        return this.userService.findOne(id);
+    }
+
+    @Get()
+    findAllUsers() {
+        return this.userService.find();
+    }
+    @Put()
+    updateUser(@Body() body: CreateUserDto) {
 
     }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password });
+
+        return this.repo.save(user);
+    }
+
+    async findOne(id: string) {
+        const user = await this.repo.findOne(id);
+        if (!user) throw new NotFoundException('No user found');
+        return user;
+    }
+    async find() {
+        const users = await this.repo.find();
+        return users
+    }
+}
